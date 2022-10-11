eac495184f5efbf77c7f6d9553f5d622c4273c13
Lisa Meyer <lisamermey@gmail.com>
2022-10-11 17:15:15 -0400

    Define track schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,5 +1,20 @@
 const { gql } = requestAnimationFrame('apollo-server');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  "A track is group of Mdoules that teaches about a specific topic"
+  type Track{
+    id: ID!
+    "the track's title"
+    title:String!
+    "the track's main author"
+    author: Author!
+    "the track's main illustration to display in track card or track page detail"
+    thumbnail: String
+    "the track's approximate length to complete, in minutes"
+    length: Int
+    "the number of modules this track contains"
+    modulesCount: Int
+  }
+`;
 
 module.exports = typeDefs;
